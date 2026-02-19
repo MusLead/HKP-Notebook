a61d045fadaf1e9e97e71b9529f827403c37e0f7
Agha Aslam <aghaaslam246@gmail.com>
2026-02-19 11:40:25 +0000

    Monad examples from Youtbe

---
+++ MonadDemo.hs
@@ -0,0 +1,48 @@
+-- What is Monad? 
+-- Computherphile (21:50 Minutes):  https://youtu.be/t1e8gqXLbsU?si=D_km5tI2EhrFlhIB
+-- A Byte of Code (2:30 Minutes): https://youtu.be/VgA4wCaxp-Q?si=HK1InfYfWahIJFBe
+
+data Expr
+  = Val Int
+  | Div Expr Expr
+  deriving (Show, Eq)
+
+safediv :: Int -> Int -> Maybe Int
+safediv n m =
+  if m == 0
+    then Nothing
+    else Just (n `div` m)
+
+-- nested case version
+evalCase :: Expr -> Maybe Int
+evalCase (Val n) = Just n
+evalCase (Div x y) =
+  case evalCase x of
+    Nothing -> Nothing
+    Just n ->
+      case evalCase y of
+        Nothing -> Nothing
+        Just m  -> safediv n m
+
+-- explicit bind
+bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+bindMaybe m f =
+  case m of
+    Nothing -> Nothing
+    Just x  -> f x
+
+-- >>= version
+evalBind :: Expr -> Maybe Int
+evalBind (Val n) = return n
+evalBind (Div x y) =
+  evalBind x >>= (\n ->
+    evalBind y >>= (\m ->
+      safediv n m))
+
+-- do notation version
+eval :: Expr -> Maybe Int
+eval (Val n) = return n
+eval (Div x y) = do
+  n <- eval x
+  m <- eval y
+  safediv n m
